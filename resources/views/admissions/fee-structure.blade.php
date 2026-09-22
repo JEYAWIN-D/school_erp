aecec70775fc21151c9fe87f061db0511e79c3a4
MithulWP <mithulpranav007@gmail.com>
2026-09-22 19:24:27 +0530

    fix(ui): remove duplicate plus icon on New Admission button

--- resources/views/admissions/fee-structure.blade.php
+++ resources/views/admissions/fee-structure.blade.php
@@ -73,7 +73,7 @@
         <svg class="w-4 h-4 text-[#8C2826]" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2.5" d="M12 4v16m8-8H4"/>
         </svg>
-        <span>+ New Admission</span>
+        <span>New Admission</span>
       </a>
     </div>
   </div>
