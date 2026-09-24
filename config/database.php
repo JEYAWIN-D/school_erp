8cc4c7d77db4dcff17956a529c4e96b404272a7b
JEYAWIN-D <jeyawind@gmail.com>
2026-09-24 22:32:50 +0530

    fix(database): remove ATTR_EMULATE_PREPARES to prevent PostgreSQL boolean integer type mismatch

--- config/database.php
+++ config/database.php
@@ -96,10 +96,6 @@ return [
             'prefix_indexes' => true,
             'search_path' => 'public',
             'sslmode' => env('DB_SSLMODE', 'require'),
-            'options' => array_filter([
-                PDO::ATTR_PERSISTENT => env('DB_PERSISTENT', true),
-                PDO::ATTR_EMULATE_PREPARES => true,
-            ]),
         ],
 
         'sqlsrv' => [
